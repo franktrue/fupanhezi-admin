1eb2f8326d64491435848ea572c09e7bf76fcdc8
franktrue <fnf@yexingxia.com>
2023-06-08 15:24:51 +0800

    添加清楚题材相关缓存

--- backend/stock/services/trade_date.py
+++ backend/stock/services/trade_date.py
@@ -16,7 +16,7 @@ class StockTradeDateService():
             cursor.execute('TRUNCATE TABLE {0}'.format(self.table_name))
         trade_date.to_sql(name=self.table_name, con=self.engine, if_exists="append", index=False)
 
-    # 清空前端表格缓存（最近的20个工作日）
+    # 清空前端缓存（最近的20个工作日）
     def clearCache(self):
         today = datetime.date.today()
         # 获取20天交易日
@@ -24,3 +24,6 @@ class StockTradeDateService():
         prefix = "cache:fupanhezi:stockZtHistory:ztRow_"
         for trade_date in trade_date_range:
             delete_cache_by_prefix(prefix=prefix+trade_date.trade_date.strftime("%Y-%m-%d"))
+
+        # 题材相关API缓存
+        delete_cache_by_prefix(prefix="cache:fupanhezi:stockBoardMap:Board:")
