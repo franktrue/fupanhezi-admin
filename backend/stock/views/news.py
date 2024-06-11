ee61f5ac874b19b83bc8db2d28ed3ccd89ece072
franktrue <fnf@yexingxia.com>
2024-06-11 14:11:55 +0800

    优化消息面缓存

--- backend/stock/views/news.py
+++ backend/stock/views/news.py
@@ -42,6 +42,20 @@ class StockNewsViewSet(CustomModelViewSet):
     cache_key = "cache:fupanhezi:stockNews:id:"
     cache_tag_key = "cache:fupanhezi:stockNewsTag:id:"
 
+    def update(self, request, *args, **kwargs):
+        partial = kwargs.pop('partial', False)
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data, request=request, partial=partial)
+        serializer.is_valid(raise_exception=True)
+        self.perform_update(serializer)
+
+        if getattr(instance, '_prefetched_objects_cache', None):
+            # If 'prefetch_related' has been applied to a queryset, we need to
+            # forcibly invalidate the prefetch cache on the instance.
+            instance._prefetched_objects_cache = {}
+        delete_cache_by_key("{0}{1}".format(self.cache_key, instance.id))
+        return DetailResponse(data=serializer.data, msg="更新成功")
+
     def destroy(self, request, *args, **kwargs):
         instance = self.get_object()
         tags = StockNewsTag.objects.filter(news_id=instance.id).all()
